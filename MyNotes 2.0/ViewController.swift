054e31dbbfe2985e6d600d657e172cab7ea0a8eb
Sergey <a@a.com>
2021-03-19 18:19:42 +0700

    Initial Commit

---
+++ MyNotes 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes 2.0
+//
+//  Created by Sergey Golovin on 19.03.2021.
+//  Copyright © 2021 GoldenWindGames LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
